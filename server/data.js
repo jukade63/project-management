a05e0fdef3e8b211b60f7cc82d0490fb46aafb21
Your Name <you@example.com>
2023-05-22 14:45:39 +0700

    project and client query done

---
+++ server/data.js
@@ -0,0 +1,76 @@
+const projects = [
+  {
+    id: "1",
+    clientId: "1",
+    name: "Brave Tiger",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    status: "In Progress",
+  },
+  {
+    id: "2",
+    clientId: "2",
+    name: "Dazzling Rainbow",
+    description:
+      "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    status: "Completed",
+  },
+  {
+    id: "3",
+    clientId: "3",
+    name: "Whispering Willow",
+    description:
+      "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.",
+    status: "In Progress",
+  },
+  {
+    id: "4",
+    clientId: "4",
+    name: "Enigmatic Phoenix",
+    description:
+      "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore.",
+    status: "Pending",
+  },
+  {
+    id: "5",
+    clientId: "5",
+    name: "Radiant Orchid",
+    description:
+      "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia.",
+    status: "In Progress",
+  },
+];
+
+const clients = [
+  {
+    id: "1",
+    name: "John Doe",
+    email: "johndoe@example.com",
+    phone: "+1 (555) 123-4567",
+  },
+  {
+    id: "2",
+    name: "Jane Smith",
+    email: "janesmith@example.com",
+    phone: "+1 (555) 987-6543",
+  },
+  {
+    id: "3",
+    name: "Alice Johnson",
+    email: "alicejohnson@example.com",
+    phone: "+1 (555) 246-8135",
+  },
+  {
+    id: "4",
+    name: "Michael Brown",
+    email: "michaelbrown@example.com",
+    phone: "+1 (555) 135-7924",
+  },
+  {
+    id: "5",
+    name: "Emily Davis",
+    email: "emilydavis@example.com",
+    phone: "+1 (555) 864-2097",
+  },
+];
+
+module.exports = { projects, clients };
